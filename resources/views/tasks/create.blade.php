7596cdefa1a3caf3e7dedc72c323a1f0daa38a3b
Manatsu <k.manatsu87@gmail.com>
2017-07-13 23:25:22 +0000

    bootstrup design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -10,6 +10,7 @@
      <div class="col-xs-12">   
      <div class="col-lg-offset-3 col-lg-6">  
      <div class="col-lg-offset-2 col-md-8">
+     <div class="col-lg-offset-2 col-sm-8">     
         {!! Form::model($task,['route' => 'tasks.store']) !!}
         
         <div class="form-group">    
@@ -27,5 +28,6 @@
     </div>
     </div>
     </div>
+    </di>
 </div>
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -9,6 +9,7 @@
         <div class="col-xs-12">   
         <div class="col-lg-offset-3 col-lg-6">  
         <div class="col-lg-offset-2 col-md-8">
+        <div class="col-lg-offset-2 col-sm-8">     
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
     <div class="form-group">
         {!! Form::label('task', 'タスク:') !!}
@@ -19,8 +20,8 @@
          {!! Form::text('status') !!}
     </div>
         {!! Form::submit('更新' ,['class' => 'btn btn-default']) !!}
-
     {!! Form::close() !!}
+        </div>
         </div>
         </div>
         </div>
