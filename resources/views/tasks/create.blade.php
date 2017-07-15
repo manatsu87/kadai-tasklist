a6e4087de62dcacc81c685d7b3ed3e3fb933bf51
Manatsu <k.manatsu87@gmail.com>
2017-07-15 01:41:32 +0000

    bootstrup syusei

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -7,10 +7,7 @@
         
         
  <div class="row">
-     <div class="col-xs-12">   
-     <div class="col-lg-offset-3 col-lg-6">  
-     <div class="col-lg-offset-2 col-md-8">
-     <div class="col-lg-offset-2 col-sm-8">     
+   <div class="col-xs-12 col-lg-offset-3 col-lg-6 col-lg-offset-2 col-md-8 col-lg-offset-2 col-sm-8">    
         {!! Form::model($task,['route' => 'tasks.store']) !!}
         
         <div class="form-group">    
@@ -26,6 +23,8 @@
         
          {!! Form::close() !!}  
     </div>
-
+    </div>
+    </div>
+    </div>
 </div>
 @endsection
