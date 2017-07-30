995db90cb963f28efb9e1e5bd8cfa1f5a0c6d2ad
Manatsu <k.manatsu87@gmail.com>
2017-07-30 03:51:41 +0000

    login

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,5 +11,15 @@
 |
 */
 
-Route::get('/', 'TasksController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::resource('tasks', 'TasksController');
+
+// ユーザ登録
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
